e3f49fab30029995155da1dcb6cf9d24e33e008a
geraldineeleazar <geraldine@osaro.com>
2020-01-10 14:20:06 -0800

    deleted robots

--- robot_booking/FreeBusy_Booking.py
+++ robot_booking/FreeBusy_Booking.py
@@ -1,14 +1,3 @@
-robots = {
-    'VS': 'osaro.com_3138373730323034343235@resource.calendar.google.com',
-    'LR-MATE': 'osaro.com_3438343531333034393639@resource.calendar.google.com',
-    'M-20': 'osaro.com_32373736333937323031@resource.calendar.google.com',
-    'IIWA': 'osaro.com_32323831353732373733@resource.calendar.google.com',
-    'KR10': 'osaro.com_3137363632333931383736@resource.calendar.google.com',
-    'UR10': 'osaro.com_383639353138393738@resource.calendar.google.com',
-    'YASK': 'osaro.com_1887m7gjkdtjkiaoj5c7kc6rrtcg06gb6or3ie1i6gqj8d1p6s@resource.calendar.google.com',
-    'KWSK': 'osaro.com_3637383431393334343032@resource.calendar.google.com',
-    'IRB': 'osaro.com_3234333336343235363734@resource.calendar.google.com'
-}
 class FreebusyBooking:
     '''Where booking takes place. Currently books the next available timeslot in given time range'''
     def __init__(self, robot_id, robots, service):
